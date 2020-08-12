2f6f3c9eea4afd2ac2a2176e1e2fe1cbf7028d56
Naomi <56322130+NaomiNaidoo@users.noreply.github.com>
2020-08-12 11:50:51 +0200

    Add project files.

---
+++ Del2View2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Del2View2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
